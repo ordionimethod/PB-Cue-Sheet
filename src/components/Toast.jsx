75e27ec90ab55e79266becc205b807044f33f802
ordionimethod <celine@theordionimethod.com>
2026-09-04 10:10:41 -0700

    Delete src directory

--- src/components/Toast.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Toast({ message, isError, visible }) {
-  return (
-    <div id="toast" className={`${visible ? 'show' : ''} ${isError ? 'error' : ''}`}>
-      {message}
-    </div>
-  );
-}
